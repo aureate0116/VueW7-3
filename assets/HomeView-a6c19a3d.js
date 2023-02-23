28db498a722a617429ac739430762d85b18a0df9
aureate0116 <aureate0116@gmail.com>
2023-02-23 20:51:11 +0800

    deploy

---
+++ assets/HomeView-a6c19a3d.js
@@ -0,0 +1,1 @@
+import{_ as e,o,c as s,a as t}from"./index-4c5aa5b2.js";const c={components:{}},a={class:"home"},n=t("h1",{class:"text-primary"},"Hello, This is Home Page.",-1),r=[n];function _(i,p,d,m,l,h){return o(),s("div",a,r)}const u=e(c,[["render",_]]);export{u as default};
